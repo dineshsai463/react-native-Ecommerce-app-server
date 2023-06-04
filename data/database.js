fda582b17f8a9032ed5ace8b82d8fa54ee3c3be9
dineshsai463 <dineshsai463@example.com>
2023-06-04 13:40:10 +0530

    heroku alternate

--- data/database.js
+++ data/database.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-     mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
-        console.log("mongodb is connected")
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "UdemyCourse",
     });
 
-    console.log(`Server connected to database `);
+    console.log(`Server connected to database ${connection.host}`);
   } catch (error) {
     console.log("Some Error Occurred", error);
     process.exit(1);
